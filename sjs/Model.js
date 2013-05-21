ddb7ffffb413b7a93c92f3f90ab40bc0b68adb8c
Lyle Troxell <lyle@troxell.com>
2013-05-21 15:50:10 -0700

    Initial commit

---
+++ sjs/Model.js
@@ -0,0 +1,28 @@
+﻿
+guidedModel =// @startlock
+{
+	Person :
+	{
+		fullName :
+		{
+			onGet:function()
+			{// @endlock
+				return this.firstName + ' ' + this.lastName;
+			}// @startlock
+		},
+		email :
+		{
+			events :
+			{
+				onLoad:function(attributeName)
+				{// @endlock
+					try {
+						currentSession().checkPermission('admin');
+					}catch(e){
+						this.email = "****";
+					}
+				}// @startlock
+			}
+		}
+	}
+};// @endlock
---
+++ sjs/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,34 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var savePerson = {};	// @button
+	var createPerson = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	savePerson.click = function savePerson_click (event)// @startlock
+	{// @endlock
+		waf.sources.person.save({
+			onSuccess: function(event){
+				//console.log(event);
+				alert('Thanks ' + event.dataSource.fullName);
+				$('#personForm').hide();
+			},
+			onError: function(event){
+				alert("Oh drat! \n" + event.error[0].message);
+			}
+		});
+	};// @lock
+
+	createPerson.click = function createPerson_click (event)// @startlock
+	{// @endlock
+		$('#personForm').show();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("savePerson", "click", savePerson.click, "WAF");
+	WAF.addListener("createPerson", "click", createPerson.click, "WAF");
+// @endregion
+};// @endlock
